e42ceb0a544a921ac69033186ef20f1e00633d2c
Robson Juliano <robsonj2011@gmail.com>
2021-03-23 14:43:03 -0300

    Mostrar Situação

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [height, setHeight] = useState(1);
   const [weight, setWeight] = useState(0);
   const [bmi, setBMI] = useState(0);
+  const [situation, setSIT] = useState(1);
 
   function handleHeight(e) {
     let h = e.target.value;
@@ -19,8 +20,28 @@ function App() {
 
     let bmi1 = (weight / (height * 2)) * 100;
     setBMI(bmi1.toFixed(2));
+    if (bmi1 < 1) {
+      setSIT("Preencha os dados.")
+      console.log("Preencha Altura e Peso")
+    }
+      else if (bmi1 < 18.5) {
+        setSIT("Magreza")
+        console.log("Magreza")
+      } else if (bmi1 >= 18.5 && bmi1 <= 25) {
+        setSIT("Normal")
+        console.log("Normal")
+      } else if (bmi1 > 25 && bmi1 < 30) {
+        setSIT("Sobrepeso")
+        console.log("Sobrepeso")
+      } else if (bmi1 > 30 && bmi1 < 40) {
+        setSIT("Obesidade")
+        console.log("Obesidade")
+      } else {
+        setSIT("Obesidade Grave")
+        console.log("Obesidade Grave")
+      }
 
-  }, [height, weight]);
+    }, [height, weight]);
 
 
 
@@ -45,7 +66,7 @@ function App() {
                   <div className="form-group">
                     <input type="number" onChange={handleWeight} placeholder="Peso"></input>
                     <h2><br></br>IMC :{bmi}</h2>
-
+                    <h1>{situation}</h1>
                   </div>
                 </form>
               </div>
