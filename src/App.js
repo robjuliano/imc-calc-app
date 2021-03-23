38f0c945d46fb9ea484dcd2224e74e973ad6dc11
Robson Juliano <robsonj2011@gmail.com>
2021-03-23 12:48:03 -0300

    Calculadora IMC

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './css/Calc.css'
+
+function App() {
+  const [height, setHeight] = useState(1);
+  const [weight, setWeight] = useState(0);
+  const [bmi, setBMI] = useState(0);
+
+  function handleHeight(e) {
+    let h = e.target.value;
+    setHeight(h);
+  }
+  function handleWeight(e) {
+    let w = e.target.value;
+    setWeight(w);
+  }
+  useEffect(() => {
+
+    let bmi1 = (weight / (height * 2)) * 100;
+    setBMI(bmi1.toFixed(2));
+
+  }, [height, weight]);
+
+
+
+
+
+  return (
+
+
+    <section id="cover" className="min-vh-100" >
+      <div id="cover-caption">
+        <div className="container">
+          <div className="row text-white">
+            <div className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
+              <h1 className="display-4 py-2 text-truncate">Calculadora IMC</h1>
+              <div className="px-2">
+                <form action="" className="justify-content-center">
+
+
+                  <div className="form-group">
+                    <input type="number" onChange={handleHeight} placeholder="Altura"></input>
+                  </div>
+                  <div className="form-group">
+                    <input type="number" onChange={handleWeight} placeholder="Peso"></input>
+                    <h2><br></br>IMC :{bmi}</h2>
+
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  );
+}
+
+export default App;
